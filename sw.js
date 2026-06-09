7ac303f64a82c67cdb1f40b8642394fe8d676b18
shimihillel <shimrit.assaf@gmail.com>
2026-06-09 17:39:44 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'shimi-looks-v1';
-const ASSETS = ['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./assets/icon-32.png','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
