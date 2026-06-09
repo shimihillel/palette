a9e97a3968d72213e7016f4e76de11d4404ba8d7
shimihillel <shimrit.assaf@gmail.com>
2026-06-09 19:31:57 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+// Cache disabled for this version to avoid old GitHub Pages service-worker files.
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
+});
