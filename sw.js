4e86cf9548ea04c13df141cf6c01a57b2b0187e5
shimihillel <shimrit.assaf@gmail.com>
2026-07-09 14:42:33 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// v41 cache reset
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))); });
-self.addEventListener('fetch', () => {});
